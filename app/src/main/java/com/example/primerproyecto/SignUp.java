ccaf5d2fbb148453e3bb8fb15985626852a0fb49
korah91 <joel.upv>
2023-03-14 16:45:42 +0100

    cambiado el tamaño de un texto

--- app/src/main/java/com/example/primerproyecto/SignUp.java
+++ app/src/main/java/com/example/primerproyecto/SignUp.java
@@ -1,11 +1,16 @@
 package com.example.primerproyecto;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +18,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class SignUp extends AppCompatActivity {
 
     EditText et_email, et_password;
-    Button btn_signup;
+    Button btn_signup, btn_cambiarIdioma, btn_guest;
     TextView tv_login;
 
     @Override
@@ -36,7 +43,8 @@ public class SignUp extends AppCompatActivity {
         et_password = findViewById(R.id.et_password);
         btn_signup = findViewById(R.id.btn_signup);
         tv_login = findViewById(R.id.tv_login);
-
+        btn_cambiarIdioma = findViewById(R.id.btn_cambiarIdioma);
+        btn_guest = findViewById(R.id.btn_guest);
 
         // Cuando se pulsa el boton para iniciar sesion
         // Primero se comprueba que no esten vacios los campos
@@ -78,6 +86,82 @@ public class SignUp extends AppCompatActivity {
                         Toast.makeText(SignUp.this, getString(R.string.yaExisteEmail), Toast.LENGTH_LONG).show();
                     }
                 }
+            }
+
+        });
+
+
+        // Boton para acceder como Invitado
+        btn_guest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SignUp.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+        // Cuando se pulsa el boton de cambiar idioma se muestra un dialog con los idiomas disponibles
+        btn_cambiarIdioma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // cambiar idioma
+                AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+                builder.setTitle(getString(R.string.eligeIdioma));
+                CharSequence[] opciones = {"Castellano", "English"};
+                builder.setItems(opciones, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Locale locale;
+                        Resources res;
+                        DisplayMetrics dm;
+                        Configuration conf;
+                        Intent refresh;
+                        switch (i) {
+                            // Español
+                            case 0:
+                                locale = new Locale("es-rEs");
+                                res = getResources();
+                                dm = res.getDisplayMetrics();
+                                conf = res.getConfiguration();
+                                conf.locale = locale;
+                                res.updateConfiguration(conf, dm);
+
+                                // Se reinicia la actividad con un nuevo nombre
+                                refresh = new Intent(SignUp.this, SignUp.class);
+
+                                // Se crea un toast
+                                Toast.makeText(SignUp.this, "Castellano", Toast.LENGTH_SHORT).show();
+                                startActivity(refresh);
+                                finish();
+
+                                break;
+                            case 1:
+                                locale = new Locale("en");
+                                res = getResources();
+                                dm = res.getDisplayMetrics();
+                                conf = res.getConfiguration();
+                                conf.locale = locale;
+                                res.updateConfiguration(conf, dm);
+
+                                // Se reinicia la actividad con un nuevo nombre
+                                refresh = new Intent(SignUp.this, SignUp.class);
+
+                                // Se crea un toast
+                                Toast.makeText(SignUp.this, "English", Toast.LENGTH_SHORT).show();
+
+
+                                startActivity(refresh);
+                                finish();
+                                break;
+
+                        }
+
+                    }
+                });
+                builder.show();
             }
         });
     }
